dd20f6cd0dddb6b650f1e9ef8a5f48ab7ea2faed
Kokorin-Savage <kokorin0521@gmail.com>
2021-05-09 15:57:48 +0300

    add lab2

---
+++ lab_2/Form1.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace lab_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.log = new System.Windows.Forms.TextBox();
+            this.firstWord = new System.Windows.Forms.TextBox();
+            this.secondWord = new System.Windows.Forms.TextBox();
+            this.inOutBox = new System.Windows.Forms.RichTextBox();
+            this.reset = new System.Windows.Forms.Button();
+            this.findFirstWord = new System.Windows.Forms.Button();
+            this.findSecondWord = new System.Windows.Forms.Button();
+            this.change = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // log
+            // 
+            this.log.Location = new System.Drawing.Point(12, 9);
+            this.log.Multiline = true;
+            this.log.Name = "log";
+            this.log.Size = new System.Drawing.Size(259, 400);
+            this.log.TabIndex = 0;
+            // 
+            // firstWord
+            // 
+            this.firstWord.Location = new System.Drawing.Point(277, 43);
+            this.firstWord.Name = "firstWord";
+            this.firstWord.Size = new System.Drawing.Size(222, 20);
+            this.firstWord.TabIndex = 1;
+            // 
+            // secondWord
+            // 
+            this.secondWord.Location = new System.Drawing.Point(277, 100);
+            this.secondWord.Name = "secondWord";
+            this.secondWord.Size = new System.Drawing.Size(222, 20);
+            this.secondWord.TabIndex = 2;
+            // 
+            // inOutBox
+            // 
+            this.inOutBox.Location = new System.Drawing.Point(505, 12);
+            this.inOutBox.Name = "inOutBox";
+            this.inOutBox.Size = new System.Drawing.Size(262, 397);
+            this.inOutBox.TabIndex = 3;
+            this.inOutBox.Text = "";
+            this.inOutBox.KeyDown += new System.Windows.Forms.KeyEventHandler(this.ButtonPIsPress);
+            // 
+            // reset
+            // 
+            this.reset.Location = new System.Drawing.Point(277, 12);
+            this.reset.Name = "reset";
+            this.reset.Size = new System.Drawing.Size(222, 25);
+            this.reset.TabIndex = 4;
+            this.reset.Text = "Сброс";
+            this.reset.UseVisualStyleBackColor = true;
+            this.reset.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // findFirstWord
+            // 
+            this.findFirstWord.Location = new System.Drawing.Point(277, 69);
+            this.findFirstWord.Name = "findFirstWord";
+            this.findFirstWord.Size = new System.Drawing.Size(222, 25);
+            this.findFirstWord.TabIndex = 5;
+            this.findFirstWord.Text = "Найти первое";
+            this.findFirstWord.UseVisualStyleBackColor = true;
+            this.findFirstWord.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // findSecondWord
+            // 
+            this.findSecondWord.Location = new System.Drawing.Point(277, 126);
+            this.findSecondWord.Name = "findSecondWord";
+            this.findSecondWord.Size = new System.Drawing.Size(222, 25);
+            this.findSecondWord.TabIndex = 6;
+            this.findSecondWord.Text = "Найти второе";
+            this.findSecondWord.UseVisualStyleBackColor = true;
+            this.findSecondWord.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // change
+            // 
+            this.change.Location = new System.Drawing.Point(277, 157);
+            this.change.Name = "change";
+            this.change.Size = new System.Drawing.Size(222, 25);
+            this.change.TabIndex = 7;
+            this.change.Text = "Поменять";
+            this.change.UseVisualStyleBackColor = true;
+            this.change.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.change);
+            this.Controls.Add(this.findSecondWord);
+            this.Controls.Add(this.findFirstWord);
+            this.Controls.Add(this.reset);
+            this.Controls.Add(this.inOutBox);
+            this.Controls.Add(this.secondWord);
+            this.Controls.Add(this.firstWord);
+            this.Controls.Add(this.log);
+            this.Name = "Form1";
+            this.Text = "Текстовые файлы, символы и строки";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        private System.Windows.Forms.Button change;
+
+        private System.Windows.Forms.Button reset;
+        private System.Windows.Forms.Button findFirstWord;
+        private System.Windows.Forms.Button findSecondWord;
+
+        private System.Windows.Forms.RichTextBox inOutBox;
+        private System.Windows.Forms.TextBox firstWord;
+        private System.Windows.Forms.TextBox secondWord;
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.TextBox log;
+
+        #endregion
+    }
+}
---
+++ lab_2/Form1.cs
@@ -0,0 +1,272 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab_2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            change.Enabled = false;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            System.Windows.Forms.ContextMenu contextMenu1;
+            contextMenu1 = new System.Windows.Forms.ContextMenu();
+            System.Windows.Forms.MenuItem menuItem1;
+            menuItem1 = new System.Windows.Forms.MenuItem();
+            System.Windows.Forms.MenuItem menuItem2;
+            menuItem2 = new System.Windows.Forms.MenuItem();
+            System.Windows.Forms.MenuItem menuItem3;
+            menuItem3 = new System.Windows.Forms.MenuItem();
+            contextMenu1.MenuItems.AddRange(new System.Windows.Forms.MenuItem[]
+            {
+                menuItem1, menuItem2, menuItem3
+            });
+            menuItem1.Index = 0;
+            menuItem1.Text = "Открыть";
+            menuItem2.Index = 1;
+            menuItem2.Text = "Сохранить";
+            menuItem3.Index = 2;
+            menuItem3.Text = "Сохранить как";
+            inOutBox.ContextMenu = contextMenu1;
+            menuItem1.Click += new System.EventHandler(this.menuItem1_Click);
+            menuItem2.Click += new System.EventHandler(this.menuItem2_Click);
+            menuItem3.Click += new System.EventHandler(this.menuItem3_Click);
+        }
+
+        string MyFName = "";
+
+        private void menuItem1_Click(object sender, System.EventArgs e)
+        {
+            openFileDialog1.Filter = "Текстовые файлы (*.rtf; *.txt; *.dat) | *.rtf; *.txt; *.dat";
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                MyFName = openFileDialog1.FileName;
+                inOutBox.LoadFile(MyFName);
+            }
+        }
+
+        private void menuItem2_Click(object sender, EventArgs e)
+        {
+            if (MyFName != "")
+            {
+                inOutBox.SaveFile(MyFName);
+            }
+            else
+            {
+                saveFileDialog1.Filter = "Текстовые файлы (*.rtf; *.txt; *.dat) | *.rtf; *.txt; *.dat";
+                if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+                {
+                    MyFName = saveFileDialog1.FileName;
+                    inOutBox.SaveFile(MyFName);
+                }
+            }
+        }
+
+        private void menuItem3_Click(object sender, System.EventArgs e)
+        {
+            saveFileDialog1.Filter = "Текстовые файлы (*.rtf; *.txt; *.dat) | *.rtf; *.txt; *.dat";
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                MyFName = saveFileDialog1.FileName;
+                inOutBox.SaveFile(MyFName);
+            }
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            log.Clear();
+            firstWord.Clear();
+            secondWord.Clear();
+            inOutBox.Clear();
+            findFirstWord.Enabled = true;
+            findSecondWord.Enabled = true;
+            change.Enabled = true;
+        }
+
+        int result1, result2;
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int LenText;
+            log.Text += "Поиск первого слова" + Environment.NewLine;
+            String FWord = firstWord.Text.ToString();
+            LenText = inOutBox.Text.Length;
+            result1 = FindWord(FWord, LenText);
+            if (result1 != -1)
+            {
+                log.Text += "Позиция первого слова: " + (result1 + 1) + Environment.NewLine + Environment.NewLine;
+                inOutBox.SelectionStart = result1;
+                inOutBox.SelectionLength = FWord.Length;
+                inOutBox.SelectionBackColor = Color.Red;
+                findFirstWord.Enabled = false;
+                if (findSecondWord.Enabled == false)
+                {
+                    change.Enabled = true;
+                }
+            }
+            else
+            {
+                log.Text += "Слово не найдено " + Environment.NewLine + Environment.NewLine;
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int LenText;
+
+            log.Text += "Поиск второго слова" + Environment.NewLine;
+            String FWord = secondWord.Text.ToString();
+            LenText = inOutBox.Text.Length;
+            result2 = FindWord(FWord, LenText);
+            if (result2 != -1)
+            {
+                log.Text += "Позиция второго слова: " + (result2 + 1) + Environment.NewLine + Environment.NewLine;
+                inOutBox.SelectionStart = result2;
+                inOutBox.SelectionLength = FWord.Length;
+                inOutBox.SelectionBackColor = Color.Green;
+                findSecondWord.Enabled = false;
+                if (findFirstWord.Enabled == false)
+                {
+                    change.Enabled = true;
+                }
+            }
+            else
+            {
+                log.Text += "Слово не найдено " + Environment.NewLine + Environment.NewLine;
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (result1 < result2)
+            {
+                inOutBox.Select(result2, secondWord.Text.Length);
+                inOutBox.SelectedText = firstWord.Text.ToString();
+                inOutBox.Select(result1, firstWord.Text.Length);
+                inOutBox.SelectedText = secondWord.Text.ToString();
+                log.Text += "Произошла замена слов";
+                change.Enabled = false;
+            }
+            else
+            {
+                inOutBox.Select(result1, firstWord.Text.Length);
+                inOutBox.SelectedText = secondWord.Text.ToString();
+                inOutBox.Select(result2, secondWord.Text.Length);
+                inOutBox.SelectedText = firstWord.Text.ToString();
+                log.Text += "Произошла замена слов";
+                change.Enabled = false;
+            }
+        }
+
+        int FindWord(String FWord, int n)
+        {
+            int LenWord;
+            String ComparText;
+            LenWord = FWord.Length;
+            for (int i = 0; i <= n - LenWord; i++)
+            {
+                ComparText = inOutBox.Text.Substring(i, LenWord);
+                if (ComparText == FWord)
+                {
+                    return i;
+                }
+            }
+
+            return -1;
+        }
+
+        int xxxx;
+
+        int MaxLenght(string Text)
+        {
+            int LenText = Text.Length;
+            int max = 0;
+            int numLitt = 0;
+            int numSpace = 0;
+            for (int i = 0; i < LenText; i++)
+            {
+                string charr = inOutBox.Text.Substring(i, 1);
+                if (!int.TryParse(charr, out xxxx) && numLitt == 0) continue;
+                if (int.TryParse(charr, out xxxx))
+                {
+                    numLitt++;
+                    numLitt += numSpace;
+                    numSpace = 0;
+                }
+
+                if (charr == " " && numLitt > 0)
+                {
+                    numSpace++;
+                }
+
+                if (numLitt > max && !int.TryParse(charr, out xxxx) && charr != " ")
+                {
+                    max = numLitt;
+                    numLitt = 0;
+                    numSpace = 0;
+                }
+                else if (numLitt <= max && !int.TryParse(charr, out xxxx) && charr != " ")
+                {
+                    numLitt = 0;
+                    numSpace = 0;
+                }
+            }
+
+            if (numLitt > max) max = numLitt;
+            return max;
+        }
+
+        int max;
+        int numLett = -1;
+
+        private void ButtonPIsPress(object sender, KeyEventArgs e)
+        {
+            if (e.Alt && e.KeyValue == 80)
+            {
+                max = MaxLenght(inOutBox.Text);
+                if (max < 1)
+                {
+                    log.Text += "Цифры не найдены";
+                    return;
+                }
+
+                if (numLett != -1)
+                {
+                    inOutBox.SelectionStart = 0;
+                    inOutBox.SelectionLength = inOutBox.Text.Length;
+                    inOutBox.SelectionBackColor = Color.White;
+                }
+                while (true)
+                {
+                    numLett++;
+                    if (numLett == inOutBox.Text.Length - max+1) numLett = 0;
+                    
+                    string strr = inOutBox.Text.Substring(numLett, max);
+                    if (int.TryParse(strr.Substring(0, 1), out xxxx) &&
+                        int.TryParse(strr.Substring(max-1, 1), out xxxx))
+                    {
+                        if (ulong.TryParse(strr.Replace(" ", ""), out ulong yyyy))
+                        {
+                            inOutBox.SelectionStart = numLett;
+                            inOutBox.SelectionLength = max;
+                            inOutBox.SelectionBackColor = Color.Blue;
+                            return;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
