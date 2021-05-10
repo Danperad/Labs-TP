28debbc5fcfd773f8bd3e6017da75522c39433ce
Kokorin-Savage <kokorin0521@gmail.com>
2021-05-10 21:39:34 +0300

    add lab7_dop

---
+++ lab7_dop/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab7_dop
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            button1.Enabled = false;
+            double x1, y1, x2, y2, r2;
+            x1 = Convert.ToDouble(textBox2.Text);
+            y1 = Convert.ToDouble(textBox3.Text);
+            x2 = Convert.ToDouble(textBox4.Text);
+            y2 = Convert.ToDouble(textBox5.Text);
+            r2 = Convert.ToDouble(textBox6.Text);
+            Point tochka = new Point();
+            tochka.pointX = x1;
+            tochka.pointY = y1;
+            textBox1.Text += "Точка" + Environment.NewLine;
+            textBox1.Text += "x= " + tochka.pointX + "\t";
+            textBox1.Text += "y= " + tochka.pointY + Environment.NewLine;
+            textBox1.Text += Environment.NewLine + Environment.NewLine;
+            textBox1.Text += "Круг" + Environment.NewLine;
+            textBox1.Text += "Невозможно создать экземпляр абстрактного класса Circle "
+                             + Environment.NewLine;
+            textBox1.Text += Environment.NewLine + Environment.NewLine;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            button1.Enabled = true;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
---
+++ lab7_dop/class.cs
@@ -0,0 +1,115 @@
+﻿using System;
+
+namespace lab7_dop
+{
+    class Point
+    {
+        public Point()
+        {
+        }
+
+        protected double x, y;
+
+        public double pointX
+        {
+            get { return x; }
+            set
+            {
+                if (value > 0) x = value;
+            }
+        }
+
+        public double pointY
+        {
+            get { return y; }
+            set
+            {
+                if (value > 0) y = value;
+            }
+        }
+    }
+
+    abstract class Circle : Point
+    {
+        public Circle()
+        {
+        }
+
+        public Circle(double X, double Y)
+        {
+            x = X;
+            y = Y;
+        }
+
+        protected double radius;
+
+        public double Rad
+        {
+            get { return radius; }
+            set
+            {
+                if (value > 0) radius = value;
+            }
+        }
+        
+
+        public double square()
+        {
+            return Math.PI * radius * radius;
+        }
+    }
+
+    abstract class Arc : Circle
+    {
+        public Arc()
+        {
+        }
+
+        public Arc(double X, double Y, double R, double G) : base(X, Y)
+        {
+            gradus = G;
+        }
+        protected double gradus;
+        public double Grad
+        {
+            get { return gradus; }
+            set
+            {
+                if (value > 0) gradus = value;
+            }
+        }
+        
+        public abstract double lenght();
+        public abstract double perimetr();
+        public abstract double square();
+    }
+
+    class Segment : Arc
+    {
+        public Segment()
+        {
+        }
+
+        public Segment(double X, double Y, double R, int G) : base(X, Y, R, G)
+        {
+        }
+
+        private double gradRad() {
+            return (Math.PI * gradus / 180);
+        }
+        public override double lenght()
+        {
+            return radius * gradRad();
+        }
+
+        public override double perimetr()
+        {
+            return lenght() + 2*radius*Math.Sin(gradRad()/2);
+        }
+
+        public override double square()
+        {
+            return (radius*radius/2)*(gradRad()-Math.Sin(gradRad()));
+        }
+    }
+}
